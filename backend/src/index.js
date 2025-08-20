f6c032c8c645a026f8201a99bee7e6c98348d8b0
IfBilal <muhammadbilaltahir06@gmail.com>
2025-08-20 21:10:09 +0500

    Final Touch

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,18 +2,11 @@ import { app } from "./app.js";
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
 
-const envFile =
-  process.env.NODE_ENV === "production"
-    ? ".env.production"
-    : process.env.NODE_ENV === "test"
-      ? ".env.test"
-      : ".env.development";
-      
 dotenv.config();
-dotenv.config({ path: envFile });
+
 
 let port = process.env.PORT || 8001;
-
+console.log(process.env.CORS_ORIGIN);
 connectDB()
   .then(() => {
     app.listen(port, () => {
